101e26cd966e1b9488bb695e0f13df78e884e963
prmk <priymakvlad420@gmail.com>
2023-10-12 18:44:13 +0300

    v1.1 check readme for more info about version

--- README.md
+++ README.md
@@ -3,3 +3,13 @@
 - Added 4 apps (My PC, Recycle Bin, Google Chrome, Microsoft Edge)
 - Added logic for current time
 - Added popup windows when clicked "My PC", "Start Menu"
+
+-------v1.1-------
+- Added front-end and logic for popup windows (My PC, Start Menu, Tray, Network)
+- Start menu has 1 popup window for "Turn OFF"
+- Network icon in footer has popup window
+- My PC Explorer has header with input field for search
+- My PC Explorer has dropdown menu for sidebar
+- My PC Explorer has dropdown menu for disks
+- You can open My PC Explorer from tray now and minimize it
+- Added animation for turn OFF menu
--- js/script.js
+++ js/script.js
@@ -1,10 +1,30 @@
 'use strict';
 
-const myPC = document.querySelector('.desktop-pc');
+const myPcBtn = document.querySelector('.desktop-pc');
 const myPcExplorer = document.querySelector('.desktop__pc');
 
-const startMenu = document.querySelector('.desktop__start');
+const exloperBtn = document.querySelector('.exloperBtn');
+
 const startMenuBtn = document.querySelector('.footer-startMenu');
+const startMenu = document.querySelector('.desktop__start');
+
+const turnOffBtn = document.querySelector('.shutdownBtn');
+const turnOffMenu = document.querySelector('.sidebar__turnon');
+
+const trayBtn = document.querySelector('.trayBtn');
+const trayMenu = document.querySelector('.footer__tray');
+
+const internetBtn = document.querySelector('.internetBtn');
+const internetMenu = document.querySelector('.footer__internet');
+
+const closeMyPcBtn = document.querySelector('.closeMyPc');
+const trayMyPcBtn = document.querySelector('.trayMyPc');
+
+const dropdownExlorerBtn = document.querySelector('.dropdownMyPcItems');
+const exlorerMyPcItems = document.querySelector('.canHide-wrapper');
+
+const dropdownDisksBtn = document.querySelector('.dropdownDisks');
+const allDisks = document.querySelector('.disks__all');
 
 const currentTimeField = document.querySelector('.current-time');
 
@@ -13,12 +33,48 @@ function currentTime(){
     return time.toLocaleString();
 }
 
-setInterval(() => currentTimeField.innerHTML = currentTime(), 1000);
+setInterval(() => currentTimeField.innerHTML = currentTime(), 500);
 
-myPC.onclick = () =>{
-    myPcExplorer.classList.toggle('_active');
+myPcBtn.onclick = () =>{
+    myPcExplorer.classList.add('_active');
+    startMenu.classList.remove('_active');
 }
 
 startMenuBtn.onclick = () =>{
     startMenu.classList.toggle('_active');
+    turnOffMenu.classList.remove('_active');
+}
+
+turnOffBtn.onclick = () =>{
+    turnOffMenu.classList.toggle('_active');
+}
+
+trayBtn.onclick = () =>{
+    trayMenu.classList.toggle('_active');
+    internetMenu.classList.remove('_active');
+}
+
+internetBtn.onclick = () =>{
+    internetMenu.classList.toggle('_active');
+    trayMenu.classList.remove('_active');
+}
+
+closeMyPcBtn.onclick = () =>{
+    myPcExplorer.classList.remove('_active');
+}
+
+exloperBtn.onclick = () =>{
+    myPcExplorer.classList.add('_active');
+}
+
+trayMyPcBtn.onclick = () =>{
+    myPcExplorer.classList.remove('_active');
+}
+
+dropdownExlorerBtn.onclick = () =>{
+    exlorerMyPcItems.classList.toggle('_hidden');
+}
+
+dropdownDisksBtn.onclick = () =>{
+    allDisks.classList.toggle('_hidden');
 }
